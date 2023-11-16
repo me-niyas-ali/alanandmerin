fffbcaa73215dd3ea489eaee8f3be0e8f81a0803
Niyas Ali <136828928+me-niyas-ali@users.noreply.github.com>
2023-11-16 13:42:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
  }
 
 
- var countDownDate = new Date("nov 11, 2023 13:37:00").getTime();
+ var countDownDate = new Date("Sep 22, 2024 12:00:00").getTime();
  const modal = document.getElementById("myModal");
 
  var x = setInterval(function() {
